a8f202885523c3c4b61d93edb8c43005b88a15b3
Aelberth Cheong <aelberth.cheong@outlook.com>
2026-06-24 23:09:48 +0700

    feat: forgot about this one also

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 PROJECT_DIR="/home/akunsialbert/Projects/Sari-Tebu"
 BACKEND_DIR="$PROJECT_DIR/backend"
 
@@ -6,14 +7,20 @@ cd $BACKEND_DIR
 
 git -C $PROJECT_DIR pull
 BUILD_VERSION=$(git -C $PROJECT_DIR rev-parse HEAD)
+
 echo "$(date --utc +%FT%TZ): Releasing new server version. $BUILD_VERSION"
+OLD_CONTAINER=$(docker ps -q --filter "name=server")
+
 echo "$(date --utc +%FT%TZ): Running Build..."
-docker compose rm -f
 docker compose build
-OLD_CONTAINER=$(docker ps -q server)
+
 echo "$(date --utc +%FT%TZ): Scaling server up..."
-BUILD_VERSION=$BUILD_VERSION docker compose up -d --no-deps --scale server=2 --no-recreate server
+docker compose up -d --no-deps --scale server=2 --no-recreate server
+
 sleep 30
+
 echo "$(date --utc +%FT%TZ): Scaling old server down..."
 docker container rm -f $OLD_CONTAINER
+
 docker compose up -d --no-deps --scale server=1 --no-recreate server
+echo "$(date --utc +%FT%TZ): Done."
